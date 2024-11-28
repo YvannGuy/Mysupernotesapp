b69afd58932a8c62ef35b814fd2a276845686192
yvann <yvann.guyonnet@gmail.com>
2024-11-28 12:05:15 +0100

    add card notes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,24 @@
+import NoteCard from "../../components/Cards/NoteCard"
 import Navbar from "../../components/Navbar/Navbar"
 
 const Home = () => {
   return (
     <>
     <Navbar />
+    <div className="container mx-auto">
+    <div>
+    <NoteCard 
+    title="Meeting on 7th April" 
+    date="3rd Apr 2024" 
+    content="Meeting on 7th April Meeting on 7th April"   
+    tags="#Meeting" 
+    isPinned={true}
+    onEdit={()=>{}}
+    onDelete={()=>{}}
+    OnPinNote={()=>{}}
+    />
+    </div>
+    </div>
     </>
   )
 }
